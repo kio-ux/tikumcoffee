47362f597386f48ad3275948ba64e7e8ab774c8d
abisha <abisha_fadhli@hotmail.com>
2021-12-21 16:03:34 +0700

    tes bish

--- app/src/main/java/com/example/tikumcoffee/MainActivity.java
+++ app/src/main/java/com/example/tikumcoffee/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //kasih tinggalkan diriku tuk slamanya, biarkan ku sendiri
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
