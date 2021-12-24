b358a4f06cb6ed8be7f6432a91acb80db6843c18
arfianjihad <jihadarfian@gmail.com>
2021-12-24 15:26:09 +0700

    carousel

--- app/src/main/java/com/example/tikumcoffee/MainActivity.java
+++ app/src/main/java/com/example/tikumcoffee/MainActivity.java
@@ -3,15 +3,35 @@ package com.example.tikumcoffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
-    // test jihad
-    //bisha pk
 
+    private int[] mImages = new int[] {
+            R.drawable.test_satu, R.drawable.test_dua, R.drawable.test_empat
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize() {
+
+        CarouselView carouselView = findViewById(R.id.carousel);
+        carouselView.setPageCount(mImages.length);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(mImages[position]);
+            }
+        });
+
+
     }
 }
