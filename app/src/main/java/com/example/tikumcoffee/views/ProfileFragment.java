f9bcaea1774235e174a2eb1580dfcf8b0d8de118
abisha <abisha_fadhli@hotmail.com>
2021-12-30 14:24:59 +0700

    halaman profile (yang username nya bisa dikonekin dari hasil sign up nya)

--- app/src/main/java/com/example/tikumcoffee/views/ProfileFragment.java
+++ app/src/main/java/com/example/tikumcoffee/views/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.tikumcoffee.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tikumcoffee.R;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener{
+    TextView usernameText;
+    Button logoutButton;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -18,7 +23,23 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+
+        logoutButton = view.findViewById(R.id.logoutButton);
+        usernameText = view.findViewById(R.id.usernameProfileText);
+
+        logoutButton.setOnClickListener(this);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == logoutButton){
+            Intent logout = new Intent(getActivity(), LoginActivity.class);
+            startActivity(logout);
+        }
     }
 }
